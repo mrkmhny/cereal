0d2ec72006a219b9a6b7579f173f8ddee2f20a67
Mark Mahoney <mark.r.mahoney@gmail.com>
2018-02-28 19:43:49 -0500

    Normalize values based on weight

--- cereal.m
+++ cereal.m
@@ -15,12 +15,15 @@ data = data(:,[4,5,6,7,8,9,10,11,12,14,15,16]);
 
 % Use a smaller dataset for easier workflow
 % REMOVE THIS REMOVE THIS REMOVE THIS
-% data = data(1:10,:);
+data = data(1:8,:);
 
 % COLUMN NAMES for reference
-% name(1),mfr(2),type(3),calories(4),protein(5),
-% fat(6),sodium(7),fiber(8),carbo(9),sugars(10),
-% potass(11),vitamins(12),shelf(13),weight(14),cups(15),rating(16)
+% name(1),mfr(2),type(3),calories(4->1),protein(5->2),
+% fat(6->3),sodium(7->4),fiber(8->5),carbo(9->6),sugars(10->7),
+% potass(11->8),vitamins(12->9),shelf(13),weight(14->10),cups(15->11),rating(16->12)
+
+% Normalize all columns to have equal weight, except for rating, of course
+[data(:,1:11) .* (1./data(:,10)), data(:,12)]
 
 % Split out training and test sets
 training = data(1:floor(size(data)(1)*0.8),:);
@@ -31,6 +34,5 @@ test = data(floor(size(data)(1)*0.8)+1:end, :);
 % testplotx = testplot(:,1)
 % testploty = testplot(:,11)
 ratings = data(:,1)
-scatter(data(:,1),data(:,12));
+scatter(data(:,5),data(:,12));
 
-pause
